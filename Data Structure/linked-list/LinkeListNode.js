e96f920b258be6e52e6a82ec66a9256d8566b9fa
Phurba Gyalzen Sherpa <sphurba861@gmail.com>
2022-09-02 11:25:58 +0545

    Linked List Node class created

---
+++ Data Structure/linked-list/LinkeListNode.js
@@ -0,0 +1,9 @@
+
+// User define class node
+export default class LinkedListNode {
+    // constructor
+    constructor(value, next = null) {
+      this.value = value;
+      this.next = next;
+    }
+}
